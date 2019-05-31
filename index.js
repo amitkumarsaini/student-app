a478e8d35440bbd2be9eb65819ae175947f4a803
amitkumarsaini <51169102+amitkumarsaini@users.noreply.github.com>
2019-05-31 18:19:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+import { createStore } from 'redux';
+const initialState = {
+    count: 8,
+    courses: []
+};
+const reducer = (state = initialState, action) => {
+    console.log(action)
+    switch (action.type) {
+        case 'INCREMENT':
+            return Object.assign({}, state, { count: state.count + 1 });
+        case 'ADDCOURSE':
+            return Object.assign({}, state, { courses: state.courses.concat(action.course) });
+        default:
+            return state;
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducer);
+export default store;
